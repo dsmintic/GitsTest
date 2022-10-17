9310ee4ec5f6bc2c38b99ba08ed772f4b5659663
Damir Smintic <dsmintic@gmail.com>
2022-10-17 15:13:34 +0200

    4 commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         Person person1 = new Person("Damir Šmintić", "Zagreb",42);
-        System.out.println(person1.getName() + " - " + person1.getCity());
-        System.out.println(person1.getName());
-        System.out.println(person1.getCity());
-        System.out.println(person1.getYear());
+        Person person2 = new Person("Denis Križak", "Zagreb", 35);
+        System.out.println(person1.getName() + " - " + person1.getCity() + " - " + person1.getYear());
+        System.out.println(person2.getName());
 
     }
 }
