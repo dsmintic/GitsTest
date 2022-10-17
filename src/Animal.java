6877b9ab0ecd163f1ea81116295e3c9ffb5eef3b
Damir Smintic <dsmintic@gmail.com>
2022-10-17 16:01:59 +0200

    4 commit  - project rename

---
+++ src/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+
+    private String name;
+    private String type;
+
+    public Animal(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+}
