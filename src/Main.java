e4257a771f13d2f2114be44a0d9b002dedcef69c
Damir Smintic <dsmintic@gmail.com>
2022-10-17 14:28:49 +0200

    My 1st commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Damir Šmintić", "Zagreb");
+        System.out.println(person1.getName() + " - " + person1.getCity());
+
+    }
+}
